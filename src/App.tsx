28e354ee446e4341627bdef4d113d9414bb7614a
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-07-30 14:46:19 -0500

    style(Interfaz): :lipstick: Primer diseño de la pantalla principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import postersFilms from './helpers/postersFilms';
+
 export const App = () => {
   return (
-    <main className="container d-flex align-items-center justify-content-center text-center text-warning">
-      <h1>Star Wars Ships</h1>
-    </main>
+    <>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <button className="btn btn-outline-warning ms-auto" type="submit">
+            Login
+          </button>
+        </div>
+      </nav>
+      <main className="container justify-content-center text-center text-warning py-4">
+        <div className="row">
+          <h1 className="mb-4 display-3">Star Wars Ships</h1>
+        </div>
+        <div className="row">
+          {postersFilms.map((film) => (
+            <div className="col-6 col-lg-4 my-3" key={film.episodeID}>
+              <div className="card">
+                <div className="card-block">
+                  <img className="card-img-top" src={film.src} alt={film.src} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
   );
 };
